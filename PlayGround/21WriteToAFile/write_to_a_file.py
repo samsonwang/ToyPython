7b321652da2a551f2db61e8330fea8d0585675f3
SamsonWang <zl.wang@foxmail.com>
2018-04-25 23:40:20 +0800

    upload practice

--- PlayGround/21WriteToAFile/write_to_a_file.py
+++ PlayGround/21WriteToAFile/write_to_a_file.py
@@ -1,4 +1,3 @@
-
 '''
 write to a file
 Take the code from the How To Decode A Website exercise (if you didn’t do it or just want to play with some different code, use the code from the solution), and instead of printing the results to a screen, write the results to a txt file. In your code, just make up a name for the file you are saving to.
@@ -14,5 +13,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
